171ae34363ca4be92a5a121c085f3da00fd50ea2
mtunique <taomeng@hustunique.com>
2014-04-11 09:06:08 +0800

    init

--- knows/knows/spiders/cnbetaSpider.py
+++ knows/knows/spiders/cnbetaSpider.py
@@ -5,7 +5,7 @@ from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
-from spider.item import ArticleItem
+from knows.items import ArticleItem
 import re
 
 
--- spider/spider/spiders/cnbetaSpider.py
+++
@@ -1,2 +0,0 @@
-__author__ = 'mtunique'
-# -*- coding: utf-8 -*-
