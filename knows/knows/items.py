7f1f2984c612199011b916357c5cd7b7dae4b15d
assuming <mingxiaowang@hustunique.com>
2014-04-14 00:28:29 +0800

    changed item.py for common use

--- knows/knows/items.py
+++ knows/knows/items.py
@@ -2,7 +2,18 @@ from scrapy.item import Item, Field
 
 
 class ArticleItem(Item):
-    title = Field()
+    #taken from which website
+    fromsite = Field()
     date = Field()
+    title = Field()
+    #link to the article
     where = Field()
+    #short summery of the article
+    desc = Field()
+
     content = Field()
+    #----------------------------------------
+    #the following are items specially used for Q&A platform
+    #----------------------------------------
+    problem = Field()
+    solution = Field()
