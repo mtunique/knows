16218a4203a1cd4478ba8b90f6b4c218800fdbc1
mtunique <taomeng@hustunique.com>
2014-04-26 18:20:21 +0800

    refactor powerContent

--- algorithm/powerContent.py
+++ algorithm/powerContent.py
@@ -1,16 +1,21 @@
 __author__ = 'mt'
 # -*- coding: utf-8 -*-
-import redis
+from dbs import redisdb
+from dbs import mongodb
 from bs4 import BeautifulSoup
 
 
-class PowerContent(object):
-    def __init__(self, content):
-        self.content = content
-        self.html = BeautifulSoup(content).html
-
-    def to_string(self, separator=u"", strip=True):
-        self.html.get_text('\n', separator=separator, strip=strip)
+def to_string(content, strip=True):
+    html = BeautifulSoup(content).html
+    return html.get_text('\n', strip=strip)
 
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
+    while True:
+        tmp, content_hash = redisdb.db.brpop('content')
+        content_html = mongodb.db.content.find_one({'_id': content_hash})['content']
+
+        string = to_string(content_html)
+
+        mongodb.db.s_content.update({'_id': content_hash},{'$set': {'s': string}}, upsert=True)
+        redisdb.db.lpush('s_content', content_hash)
