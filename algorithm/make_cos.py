30cb98172173a0acd82471b34ce2a35048c658b1
mtunique <taomeng@hustunique.com>
2014-05-05 23:31:48 +0800

    add cos

--- algorithm/make_cos.py
+++ algorithm/make_cos.py
@@ -2,6 +2,23 @@ __author__ = 'mt'
 # -*- coding: utf-8 -*-
 
 
+class Vector(object):
+    def __init__(self, l):
+        self.l = l
+
+
+
+def cos(x, y):
+    if len(x) == len(y):
+        ans = 0
+
+        for i in range(len(x)):
+            ans += x[i] * y[i]
+    else:
+        print "Vectors' lengths are different"
+        return
+
+
 def main():
     pass
 
