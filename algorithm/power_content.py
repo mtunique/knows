461d8b765b64c116a2e67d154d1249629dc340b7
mtunique <taomeng@hustunique.com>
2014-05-09 07:16:33 +0800

    edit power_content

--- algorithm/power_content.py
+++ algorithm/power_content.py
@@ -9,7 +9,7 @@ def to_string(content, strip=True):
     return BeautifulSoup(content).html.body.get_text('\n', strip=strip)
 
 
-if __name__ == '__main__':
+def main():
     while True:
         tmp, content_hash = redisdb.db.brpop('content')
         content_html = mongodb.db.content.find_one({'_id': content_hash})['content']
@@ -19,3 +19,10 @@ if __name__ == '__main__':
         time = mongodb.db.article.find_one({'_id': content_hash})['time']
         mongodb.db.s_content.update({'_id': content_hash}, {'$set': {'s': string, 'time': time}}, upsert=True)
         redisdb.db.lpush('s_content', content_hash)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as err:
+        print err.message
