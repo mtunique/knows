e0f21c6c42fd840f83fd400ec879be607b7b8af2
mtunique <taomeng@hustunique.com>
2014-05-30 22:03:16 +0800

    edit handle_user

--- server/handle_user.py
+++ server/handle_user.py
@@ -7,5 +7,5 @@ def add_user(uid):
     pass
 
 
-def merge_user(uid, merge_id, type):
-    mongodb.db.user.update({'_id':uid}, {'$set':{type: merge_id}}, upsert=True)
+def merge_user(uid, merge_id, tp):
+    mongodb.db.user.update({'_id':uid}, {'$set':{tp: merge_id}}, upsert=True)
