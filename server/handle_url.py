0603402f74d40bb28f7344df81cc7c81d37ab8f8
mtunique <taomeng@hustunique.com>
2014-06-05 14:23:45 +0800

    add likeHandle

--- server/handle_url.py
+++ server/handle_url.py
@@ -4,6 +4,7 @@ import tornado.web
 from get_data import *
 import time
 from handle_user import *
+import knows_users as k_user
 
 
 def handle_err(func):
@@ -38,11 +39,18 @@ class CollectHandler(tornado.web.RedirectHandler):
 class LikeHandler(tornado.web.RedirectHandler):
     @handle_err
     def get(self):
-        if self.request.arguments['hash'] == '1':
+        tp = ''
+        if self.request.arguments['type'][0] == '1':
             tp = '$addToSet'
-        elif self.request.arguments['hash'] == '0':
+        elif self.request.arguments['type'][0] == '0':
             tp = 'pull'
-        mongodb.db.like.update(
-            {'_id':self.request.arguments['uid']},
-            {tp: {'hash': self.request.arguments['hash']}},
-            upsert=True)
+        if tp:
+            mongodb.db.like.update(
+                {'_id':   self.request.arguments['uid'][0]},
+                {tp: {'hash': self.request.arguments['hash'][0]}},
+                upsert=True)
+            self.write(1)
+        else:
+            self.write(0)
+
+
