93404bc85c2bb221e41111bcbe653ca3b5f19830
mtunique <taomeng@hustunique.com>
2014-08-04 16:44:18 +0800

    tag the article

--- algorithm/power_content.py
+++ algorithm/power_content.py
@@ -21,35 +21,37 @@ def main():
         _vocab[word] = len(_vocab)
     while True:
         tmp, content_hash = redisdb.db.brpop('content')
+        try:
+            content_html = mongodb.db.content.find_one({'_id': content_hash})['content']
+            article = mongodb.db.article.find_one({'_id': content_hash})
 
-        content_html = mongodb.db.content.find_one({'_id': content_hash})['content']
-        article = mongodb.db.article.find_one({'_id': content_hash})
+            s = to_string(content_html)
+            #print s
+            #print article
+            if article['tag'] not in ['viewdesign', 'uidesign', 'appanalyze']:
+                tag = bs.bayes_classify(s)
+                #print tag
+                mongodb.db.article.update({'_id': content_hash}, {'$set': {'tag': tag}}, upsert=True)
+            #print content_hash
 
-        s = to_string(content_html)
-        #print s
-        #print article
-        if article['tag'] not in ['viewdesign', 'uidesign', 'appanalyze']:
-            tag = bs.bayes_classify(s)
-            print tag
-            mongodb.db.article.update({'_id': content_hash}, {'$set': {'tag': tag}}, upsert=True)
-        #print content_hash
+            time = mongodb.db.article.find_one({'_id': content_hash})['time']
+            mongodb.db.s_content.update({'_id': content_hash}, {'$set': {'s': s, 'time': time}}, upsert=True)
 
-        time = mongodb.db.article.find_one({'_id': content_hash})['time']
-        mongodb.db.s_content.update({'_id': content_hash}, {'$set': {'s': s, 'time': time}}, upsert=True)
+            #TODO make article vector
+            #print doc_to_vector(s, _vocab)
 
-        #TODO make article vector
-        #print doc_to_vector(s, _vocab)
+            #mongodb.db.v_content.update({'_id': content_hash}, {'$set': {'v': doc_to_vector(s, _vocab)}}, upsert=True)
 
-        #mongodb.db.v_content.update({'_id': content_hash}, {'$set': {'v': doc_to_vector(s, _vocab)}}, upsert=True)
-
-        redisdb.db.lpush('s_content', content_hash)
-        print 3
-        break
+            redisdb.db.lpush('s_content', content_hash)
+        except Exception as err:
+            print '[%s]:%s' % (content_hash, str(err.message))
+        #print 3
+        #break
 
 
 if __name__ == '__main__':
     outfile = open('power.log', 'w')
-    #sys.stderr = outfile
-    #sys.stdout = outfile
+    sys.stderr = outfile
+    sys.stdout = outfile
     main()
     outfile.close()
