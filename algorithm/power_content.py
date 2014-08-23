f3b7b847d4c8c22b92ca622970f6defd6737943d
mtunique <taomeng@hustunique.com>
2014-08-23 18:34:27 +0800

    [alg.power_content]no output without error

--- algorithm/power_content.py
+++ algorithm/power_content.py
@@ -34,7 +34,7 @@ def main():
             mongodb.db.s_content.update({'_id': content_hash}, {'$set': {'s': s, 'time': time}}, upsert=True)
 
             #TODO make article vector
-            print doc_to_vector(s, _vocab)
+            #print doc_to_vector(s, _vocab)
 
             mongodb.db.v_content.update({'_id': content_hash}, {'$set': {'v': doc_to_vector(s, _vocab)}}, upsert=True)
 
