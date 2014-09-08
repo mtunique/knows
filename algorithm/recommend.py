c145e5f1c96674fde5f9c57f08bc8fda1b539d57
mtunique <taomeng@hustunique.com>
2014-09-08 12:30:04 +0800

    [alg.recommend] debug recommend

--- algorithm/recommend.py
+++ algorithm/recommend.py
@@ -31,7 +31,7 @@ def update_users():
     global users
     import time
     while 1:
-        users = list(mongodb.db.find({}, {'main_id': 1, 'vector': 1, 'thr': 1}))
+        users = list(mongodb.db.merger_info.find({}, {'main_id': 1, 'vector': 1, 'thr': 1}))
         time.sleep(600)
 
 
