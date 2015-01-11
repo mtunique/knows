b960407755ac28e4423bcb96aba2b51a39ff8edd
mtunique <taomeng@hustunique.com>
2015-01-11 19:30:58 +0800

    Update LICENSE recommend2.py

--- algorithm/recommend2.py
+++ algorithm/recommend2.py
@@ -1,1 +1,51 @@
-__author__ = 'mengtao'
+__author__ = 'mt'
+import traceback
+import threading
+from dbs import redisdb, mongodb
+from std_functions import cos
+
+TAGS = ['cloud', 'develop', 'prolang', 'systemsecure', 'pm',
+        'hardware', 'news', 'viewdesign,', 'uidesign', 'appanalyze']
+
+
+def main():
+    global users
+    import random
+    while True:
+        tmp, content_hash = redisdb.db.brpop('recommend_error')
+        if random.randint(1, 5) == 1:
+            redisdb.db.lpush('base_list', content_hash)
+            redisdb.db.ltrim('base_list', 0, 199)
+        try:
+            data = mongodb.db.v_content.find_one({'_id': content_hash}, {'t': 1})
+
+            for user in users:
+                try:
+                    if user['thr'] > cos(data['t'], user['vector']) or random.randint(1, 5) == 1:
+                        redisdb.db.lpush(user['uid'], content_hash)
+                        redisdb.db.ltrim(user['uid'], 0, 199)
+                except:
+                    redisdb.db.lpush('recommend_error_user', user['uid'])
+        except Exception as err:
+            print '[%s]:%s' % (content_hash, str(err.message))
+            traceback.print_exc()
+            redisdb.db.lpush('recommend_error2', content_hash)
+
+
+def update_users():
+    global users
+    import time
+    while 1:
+        users = list(mongodb.db.merger_info.find({}, {'main_id': 1, 'vector': 1, 'thr': 1, 'uid': 1}))
+        time.sleep(600)
+
+
+if __name__ == '__main__':
+    import sys
+    outfile = open('recommend2.log', 'w')
+    sys.stderr = outfile
+    sys.stdout = outfile
+    users = list(mongodb.db.merger_info.find({}, {'main_id': 1, 'vector': 1, 'thr': 1}))
+    thread = threading.Thread(target=update_users, args=())
+    thread.start()
+    main()
